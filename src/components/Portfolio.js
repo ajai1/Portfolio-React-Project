d9484565b17e0c8035679007530198ae38d9033a
ajai1 <emailtoajai@gmail.com>
2020-08-17 10:57:09 +0530

    Portfolio opening

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
+import { Backdrop } from "@material-ui/core";
 import dev from "../images/dev.gif";
 import InstaClone from "../images/InstaClone.gif";
+import BurgerBuilder from "../images/BurgerBuilder.gif";
 
 import {
   Box,
@@ -16,16 +18,19 @@ import {
 } from "@material-ui/core";
 import Navbar from "./Navbar";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   mainContainer: {
     height: "100%",
   },
   cardContainer: {
     maxWidth: 345,
-    margin: "3rem",
     margin: "5rem auto",
   },
-});
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+  },
+}));
 
 const projects = [
   {
@@ -40,12 +45,46 @@ const projects = [
     projectDesc:
       "A clone of Instagram to showcase the images, comment on them and upload images",
   },
+  {
+    image: BurgerBuilder,
+    projectName: "Burger Builder",
+    projectDesc:
+      "An interactive Burger building app to customize and checkout the order",
+  },
 ];
 
 const Portfolio = () => {
+  const [openModal, setOpenModal] = useState(false);
+  const [project, setProject] = useState(null);
   const classes = useStyles();
+
+  const showGif = (project) => {
+    setProject(project);
+    setOpenModal(!openModal);
+  };
+  const handleClose = () => {
+    setOpenModal(false);
+  };
+
   return (
     <Box component="div" className={classes.mainContainer}>
+      {openModal ? (
+        <Backdrop
+          open={openModal}
+          onClick={handleClose}
+          className={classes.backdrop}
+        >
+          <Box component="div">
+            <CardMedia
+              component="img"
+              alt="Project"
+              height="100%"
+              style={{ padding: 10 }}
+              image={project.image}
+            />
+          </Box>
+        </Backdrop>
+      ) : null}
       <Navbar />
       <Grid container justify="center" alignItems="center">
         {projects.map((project, key) => (
@@ -53,8 +92,9 @@ const Portfolio = () => {
             <Card className={classes.cardContainer}>
               <CardActionArea>
                 <CardMedia
+                  onClick={() => showGif(project)}
                   component="img"
-                  alt="Project 1"
+                  alt="Project"
                   height="100%"
                   image={project.image}
                 />
@@ -63,7 +103,7 @@ const Portfolio = () => {
                     {project.projectName}
                   </Typography>
                   <Typography
-                    variant="body"
+                    variant="body1"
                     color="textSecondary"
                     component="p"
                   >
